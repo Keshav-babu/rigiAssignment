a03bf278e0c13ac775fbe1185a69a4b4c67e7f9b
Keshav Mandal <keshav.mandal@wakefit.co>
2024-04-16 20:14:18 +0530

    post layout

--- src/component/Post/component/PostLayout/index.js
+++ src/component/Post/component/PostLayout/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import "./style.css"
 import Post from "../PostLayout1";
-import { useNavigate } from "react-router-dom";
 
 
 
 const PostLayout = ({ post }) => {
 
-    const navigate = useNavigate();
 
     return <div key={post.id} className="post-container-layout"  
-    onClick={()=>{
-        navigate( `/post/${post.id}`,{state:post});
-    }}
+
     >
         <img 
             className="profile-picture"
--- src/container/Home/index.js
+++ src/container/Home/index.js
@@ -1,6 +1,6 @@
 
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './style.css';
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPosts, getAllUsers } from "./actions";
@@ -10,34 +10,51 @@ import PostLayout from "../../component/Post/component/PostLayout";
 import Spinner from "../../component/Spinner";
 import { Virtuoso } from "react-virtuoso";
 import { debounce } from "../../config/constants";
-// import { useH } from 'react-router-dom';
 import {useLocation, useNavigate} from 'react-router-dom'
 
 const Home = () => {
   const [page, setPage] = useState(1);
+  const [post,setPost]=useState([])
+
 
   const allUsers = useSelector((state) => state?.home);
   const allPosts = useSelector((state) => state?.home?.allPost);
 
   const dispatch = useDispatch();
-  const location =useLocation()
+  const location =useLocation();
+  const navigate = useNavigate()
+  const scrollRef = useRef();
 
-  const navigate = useNavigate();
   const loadMorePosts = async () => {
-    await debounce(2000);
+    await debounce(500);
     dispatch(getAllPosts(page));
     setPage((prevPage) => prevPage + 1);
   };
-  const scrollPosition = window.scrollY;
+
+  useEffect(()=>{
+    setPost(allPosts)
+  },[allPosts])
 
   useEffect(() => {
-    console.log("location1234",{location})
+    if(location.state){
+      const {post,scrollPosition} = location.state;  
+
+      setPost(post)
+      setTimeout(() => {
+        scrollRef.current?.scrollToIndex({index: scrollPosition,});
+      }, (10));
+    }else{
+      loadMorePosts();
+    }
+
     dispatch(getAllUsers());
-    loadMorePosts();
-    window.scroll(0, scrollPosition);
+
+    window.addEventListener('beforeunload', deletelocationState);
+
+    return () => {
+        window.removeEventListener('beforeunload', deletelocationState);
+    };
   }, []);
-
-
 
   const Footer = () => {
     return (
@@ -53,6 +70,13 @@ const Home = () => {
     )
   }
 
+  
+
+
+  const deletelocationState=()=>{
+    window.history.replaceState({}, document.title, window.location.pathname);
+}
+
 
   
   return (
@@ -62,11 +86,18 @@ const Home = () => {
         <div className="left">
           <Virtuoso
             increaseViewportBy={200}
-            data={allPosts}
+            data={post}
             endReached={loadMorePosts}
             style={{ height: '80vh' }}
-            totalCount={allPosts.length}
-            itemContent={(index) => <PostLayout  key={index} post={allPosts[index]} />}
+            totalCount={post?.length}
+            itemContent={(index) => <div
+            onClick={()=>{
+              navigate( `/post/${post[index]?.id}`,{state: {post: post,scrollPosition:index,postInfo: post[index]}});
+            }}>
+                <PostLayout  key={index} post={post[index]} />
+            </div>}
+
+            ref={scrollRef}
             components={{ Footer }}
           />
         </div>
@@ -79,12 +110,6 @@ const Home = () => {
           </div>
         )}
       </div>
-
-      {/* <a href="/post/1 ">Post</a> */}
-      <p onClick={()=>{
-        navigate('/post/1');
-        // history.push('/post/1');
-      }} >Post</p>
     </section>
   );
 };
